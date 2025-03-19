556e282d80fd2c74a199f04a35661a84ca605376
chuxian922 <tchuxian922@gmail.com>
2025-03-19 08:26:32 +0800

    .

--- app/(page)/resume/page.tsx
+++ app/(page)/resume/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Header from "@/app/components/Header";
-import { useState } from "react";
-import { pdfjs, Document, Page } from "react-pdf";
+import { pdfjs, Document } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
